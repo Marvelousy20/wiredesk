f38007b5feff2434c2ecad1454303eea1000c5ff
Marvelousy20 <afosmarvel@gmail.com>
2024-06-05 22:37:22 +0100

    making sidebar

---
+++ src/components/General/Dropdown.tsx
@@ -0,0 +1,51 @@
+"use client";
+import { useState } from "react";
+import { Transition } from "@headlessui/react";
+import { ChevronUp, ChevronDown } from "lucide-react";
+
+interface DropdownProps {
+  title: string;
+  items: any;
+}
+
+const Dropdown = ({ title, items }: DropdownProps) => {
+  const [isOpen, setIsOpen] = useState(true);
+
+  return (
+    <div className="w-full">
+      <button
+        className="flex justify-between items-center w-full p-2 focus:outline-none"
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <div className="flex items-center gap-2">
+          <span>
+            {isOpen ? <ChevronDown size={15} /> : <ChevronUp size={15} />}
+          </span>
+          <span className="text-[0.75rem]">{title}</span>
+        </div>
+
+        <div>...</div>
+      </button>
+
+      <Transition
+        show={isOpen}
+        enter="transition-all duration-300"
+        enterFrom="opacity-0 max-h-0"
+        enterTo="opacity-100 max-h-screen"
+        leave="transition-all duration-300"
+        leaveFrom="opacity-100 max-h-screen"
+        leaveTo="opacity-0 max-h-0"
+      >
+        <div className="overflow-hidden">
+          {items.map((item: string, index: number) => (
+            <div key={index} className="pl-4 border-gray-300">
+              {item}
+            </div>
+          ))}
+        </div>
+      </Transition>
+    </div>
+  );
+};
+
+export default Dropdown;
--- src/components/Sidebar/Inbox/inbox.tsx
+++ src/components/Sidebar/Inbox/inbox.tsx
@@ -1,8 +1,9 @@
 "use client";
 
 import { Mail, AtSign, UserX } from "lucide-react";
-import { title } from "process";
+import Dropdown from "@/components/General/Dropdown";
 import { useState } from "react";
+import Image from "next/image";
 
 const messages = [
   {
@@ -45,6 +46,56 @@ const teams = [
   },
 ];
 
+const teamsmates = [
+  {
+    title: "James Ander...",
+    unread: "3",
+    icon: (
+      <Image
+        src="/sidebar/items/avatar.svg"
+        alt="avatar"
+        width={18}
+        height={18}
+      />
+    ),
+  },
+  {
+    title: "Alexander Tho...",
+    unread: "2",
+    icon: (
+      <Image
+        src="/sidebar/items/avatar.svg"
+        alt="avatar"
+        width={18}
+        height={18}
+      />
+    ),
+  },
+];
+
+const tickets = [
+  {
+    title: "My Ticket",
+    unread: "1",
+    icon: <UserX size={16} />,
+  },
+  {
+    title: "Support Request",
+    unread: "18",
+    icon: <UserX size={16} />,
+  },
+  {
+    title: "Support Request",
+    unread: "66",
+    icon: <UserX size={16} />,
+  },
+  {
+    title: "Bug Report",
+    unread: "5",
+    icon: <UserX size={16} />,
+  },
+];
+
 export default function Inbox() {
   const [selectedInbox, setSelectedInbox] = useState("Inbox");
 
@@ -54,7 +105,7 @@ export default function Inbox() {
         {messages.map((message, id) => (
           <div
             key={id}
-            className={`flex items-center justify-between p-1.5 opacity-75 ${
+            className={`flex items-center justify-between p-2 opacity-75 ${
               selectedInbox === message.title &&
               "bg-[#EEEFF1] border-l-4 border-black opacity-100"
             }`}
@@ -77,8 +128,85 @@ export default function Inbox() {
         ))}
       </div>
 
-      <div>
-        
+      {/* Teams */}
+      <div className="mt-4">
+        <Dropdown
+          title="Teams"
+          items={teams.map((team, id) => (
+            <div
+              key={id}
+              className="flex justify-between items-center p-2 opacity-75"
+            >
+              <div className="flex items-center gap-1.5 text-sm font-medium">
+                <p>{team.icon}</p>
+                <p>{team.title}</p>
+              </div>
+
+              <p
+                className={`text-[0.75rem] ${
+                  selectedInbox === team.title &&
+                  "bg-black text-white px-1 rounded-md"
+                }`}
+              >
+                {team.unread}
+              </p>
+            </div>
+          ))}
+        />
+      </div>
+
+      {/* Teammates */}
+      <div className="mt-4">
+        <Dropdown
+          title="Teammates"
+          items={teamsmates.map((teammate, id) => (
+            <div
+              key={id}
+              className="flex justify-between items-center p-2 opacity-75"
+            >
+              <div className="flex items-center gap-1.5 text-sm font-medium">
+                <p>{teammate.icon}</p>
+                <p>{teammate.title}</p>
+              </div>
+
+              <p
+                className={`text-[0.75rem] ${
+                  selectedInbox === teammate.title &&
+                  "bg-black text-white px-1 rounded-md"
+                }`}
+              >
+                {teammate.unread}
+              </p>
+            </div>
+          ))}
+        />
+      </div>
+
+      {/* Tickets */}
+      <div className="mt-4">
+        <Dropdown
+          title="Tickets"
+          items={tickets.map((ticket, id) => (
+            <div
+              key={id}
+              className="flex justify-between items-center p-2 opacity-75"
+            >
+              <div className="flex items-center gap-1.5 text-sm font-medium">
+                <p>{ticket.icon}</p>
+                <p>{ticket.title}</p>
+              </div>
+
+              <p
+                className={`text-[0.75rem] ${
+                  selectedInbox === ticket.title &&
+                  "bg-black text-white px-1 rounded-md"
+                }`}
+              >
+                {ticket.unread}
+              </p>
+            </div>
+          ))}
+        />
       </div>
     </div>
   );
